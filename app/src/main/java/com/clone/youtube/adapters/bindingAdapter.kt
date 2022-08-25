8d159d3e7b89099de0c35ec23380d590abe10fe3
rlarla915 <rlarla915@gmail.com>
2022-08-26 04:16:01 +0900

    using bindingadatper

---
+++ app/src/main/java/com/clone/youtube/adapters/bindingAdapter.kt
@@ -0,0 +1,10 @@
+package com.clone.youtube.adapters
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+
+@BindingAdapter("setList")
+fun setRecyclerViewListItem(recyclerView: RecyclerView){
+    recyclerView.
+}
